615a0a9dcac23d77feda17de866781dc7ad5ade0
Soufiane Mghanen <mgh.soufiane@gmail.com>
2018-02-06 09:58:01 +0000

    Change wsgi file to reflect the correct config file

--- gradua/wsgi.py
+++ gradua/wsgi.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gradua.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gradua.settings_%s" % os.environ.get('DJANGO_ENV', 'dev'))
 
 application = get_wsgi_application()
