68eb7a6fd17a8d631ddfaa0165413f93a13777e3
Soufiane Mghanen <mgh.soufiane@gmail.com>
2018-02-06 14:50:29 +0000

    Testing template index file

--- gradua/settings_prod.py
+++ gradua/settings_prod.py
@@ -39,7 +39,7 @@ STATICFILES_DIRS = [
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "frontend/public"), ],
+        'DIRS': [os.path.join(BASE_DIR, "templates"), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
